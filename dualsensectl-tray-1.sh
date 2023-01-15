be4cb5f1f4e3aa2e8cf3c1aa9cd218ce210c442b
Dimitar Filipov <grinndev@protonmail.com>
2023-01-15 21:05:17 +0200

    add shell script for controller 1

---
+++ dualsensectl-tray-1.sh
@@ -0,0 +1,17 @@
+readonly MAC_ADDRESS=''
+readonly ICON_ACTIVE='/path/to/icon.svg'
+readonly ICON_INACTIVE='/path/to/icon.svg'
+readonly TOOLTIP='Controller 1'
+
+readonly BATTERY_LEVEL=$(dualsensectl -d $MAC_ADDRESS battery | awk '{print $1}')
+
+if [ "$BATTERY_LEVEL" -ge 0 ]; then
+    echo "<img>$ICON_ACTIVE</img>"
+    echo "<txt><b>$BATTERY_LEVEL%</b></txt>"
+    echo "<txtclick>dualsensectl -d $MAC_ADDRESS power-off</txtclick>"
+    echo "<tool>$TOOLTIP</tool>" 
+else
+    echo "<img>$ICON_INACTIVE</img>"
+    echo "<tool>$TOOLTIP</tool>" 
+fi
+
